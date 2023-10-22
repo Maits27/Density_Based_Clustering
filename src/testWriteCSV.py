f3c94b3eb78617da2b0c8e0b0fb956ed8bfdafbd
Maits27 <maitiurlo@gmail.com>
2023-10-22 21:34:38 +0200

    cambios

--- src/testWriteCSV.py
+++ src/testWriteCSV.py
@@ -1,5 +1,5 @@
 import csv
 
-with open('../Barridos.csv', 'a') as file:
+with open('../out/Barridos/Barridos.csv', 'a') as file:
     writer = csv.writer(file, delimiter=',')
     writer.writerow(['pepito1', 'pepito2', 'pepito3', 'pepito4', 'pepito5', 'pepito6'])
